adf755aec76bf3ab4c630bee935e861f3e38de66
Das-108 <bishalgaihre4@gmail.com>
2025-07-10 13:49:36 +0545

    hello

---
+++ 2.py
@@ -0,0 +1,8 @@
+#write a program to detect double spaces in a string
+
+name = " sagam sigh  dhami"
+double_spaces = name.count(" ")
+print(f"Number of double spaces: {double_spaces}")
+
+# write a program to  format the following letter using escape sequence characters.
+letter = " Dear Students,\n This is python class is going well.\n thanks!"
